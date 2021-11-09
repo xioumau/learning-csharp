071d7d285ec7790e91d8de0eb1e9b5f957348dc7
Mauricio Conelhero <55670845+xioumau@users.noreply.github.com>
2021-11-08 21:40:15 -0300

    fix: "tails"

--- Logic/FlipCoin/FlipTheCoin.cs
+++ Logic/FlipCoin/FlipTheCoin.cs
@@ -5,4 +5,4 @@ In this challenge, you'll flip a coin to display either heads or tails.
 
 var coin = new Random();
 int flip = coin.Next(0, 2);
-Console.WriteLine((flip == 0) ? "heads" : tails);
+Console.WriteLine((flip == 0) ? "heads" : "tails");
